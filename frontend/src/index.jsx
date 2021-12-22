5afd92b5546d0dd208531b1be37b992dea013b4e
GabrielAlvesdeOliveira <alvesg2003@gmail.com>
2021-12-22 17:42:06 -0300

    introducting redux on app

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,4 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import promise from 'redux-promise';
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 import App from './main/app';
-ReactDOM.render(<App />, document.getElementById('root'))
+import reducers from './main/reducer'
+
+const devTools = window.__REDUX_DEVTOOLS_ENTENSIONS__ && window.__REDUX_DEVTOOLS_EXTENSIONS__()
+
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+, document.getElementById('root'))   
